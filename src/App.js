f46eb14fbc81cb8c307622a1ed47319f442b1c1f
Rodrigo Arze <rodrigo.arze@jalasoft.com>
2019-04-09 17:13:26 -0400

    Small refactor for constant variables

--- src/App.js
+++ src/App.js
@@ -3,30 +3,27 @@ import './App.css';
 import Greet from './components/greet';
 import Languages from './components/language';
 import Names from './components/nameinput'
-
+const GREETINGS = {
+  'english': 'Hello',
+  'spanish': 'Hola',
+  'italian': 'Ciao'
+}
+const LANGUAGES = ['english', 'spanish', 'italian']
 class App extends Component {
-  constructor(props) {
-    super(props)
+  constructor() {
+    super()
     this.state = {
       names: [],
-      languages: ['english', 'spanish', 'italian'],
-      greetings: {
-        'english': 'Hello',
-        'spanish': 'Hola',
-        'italian': 'Ciao'
-      },
       currentGreeting: 'Hello',
       currentLanguage: 'english'
     }
-    this.handleLanguageChange = this.handleLanguageChange.bind(this)
-    this.addName = this.addName.bind(this)
   }
 
-  handleLanguageChange(event) {
-    this.setState({currentLanguage: event.target.value, currentGreeting: this.state.greetings[event.target.value]})
+  handleLanguageChange = (event) => {
+    this.setState({currentLanguage: event.target.value, currentGreeting: GREETINGS[event.target.value]})
   }
 
-  addName(event) {
+  addName = (event) => {
     let newValues = event.target.value.split('\n')
     if (!!newValues) {
       this.setState({names: newValues.filter(value => value !== '')})
@@ -41,7 +38,7 @@ class App extends Component {
         </header>
         <main>
           <div className="App-main">
-            <Languages languages={this.state.languages} languageChange={this.handleLanguageChange}/>
+            <Languages languages={LANGUAGES} languageChange={this.handleLanguageChange}/>
             <Names addName={this.addName}/>
             <Greet greeting={this.state.currentGreeting} names={this.state.names} languageChange={this.handleLanguageChange}/>
           </div>
